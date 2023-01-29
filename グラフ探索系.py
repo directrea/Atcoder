1afaf84d54235eed42b2321fc2f20b537bb8c046
ReaTea <syunssssin@gmail.com>
2023-01-29 11:49:19 +0900

    first

---
+++ グラフ探索系.py
@@ -0,0 +1,38 @@
+from collections import deque
+import heapq
+
+
+def bfs(start):
+    global adl
+    dq = deque([start])
+    while len(dq):
+        node = dq.popleft()
+        for next_node in adl[node]:
+            # 探索する条件
+            if True:
+                dq.append(next_node)
+
+
+def dijkstra(start):
+    global costs
+    hq = [[0, start]]
+    heapq.heapify(hq)
+    costs[start] = 0
+    while len(hq):
+        cost, node = map(int, heapq.heappop(hq))  # [cost,idx]
+        for next_cost, next_node in enumerate(adl[node]):
+            sum_cost = cost+next_cost
+            if sum_cost < costs[next_node]:
+                costs[next_node] = sum_cost
+                heapq.heappush(hq, [sum_cost, next_node])
+
+
+def floyd(n):
+    global dist
+    for i in range(n):
+        dist[i][i] = 0
+    for mid in range(n):  # 中間
+        for start in range(n):  # 始点
+            for end in range(n):  # 終点
+                dist[start][end] = min(
+                    dist[start][end], dist[start][mid]+dist[mid][end])
